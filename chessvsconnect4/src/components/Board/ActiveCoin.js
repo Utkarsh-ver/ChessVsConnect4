5a34d6e1c08d3ceec437bb1a9f833fe4a42e11fe
utkarsh.ver <utkarshv2005@gmail.com>
2023-08-28 16:51:45 +0530

    latest

--- chessvsconnect4/src/components/Board/ActiveCoin.js
+++ chessvsconnect4/src/components/Board/ActiveCoin.js
@@ -1,89 +1,76 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import actionTypes from "../../reducer/actionTypes"
-import { useAppContext } from '../../context/Context'
-import { clearCandidates, makeNewMove } from '../../reducer/actions/move'
-
+import {useState,useEffect} from 'react'
+import { useAppContext } from '../../context/Context';
 
 const ActiveMarble = ({ turn, dropped, setDropped, setTurn }) => {
   const [column, setColumn] = useState(0);
   const [row, setRow] = useState();
-  const [prevColumn, setPrevColumn] = useState(null);
-  const {appState,dispatch}=useAppContext()
-  
+  const [prevColumns, setPrevColumns] = useState(null); // Keep track of previous columns
+  const { appState, dispatch } = useAppContext();
 
-  var position=appState.position[appState.position.length-1]
-
-  
+  var position = appState.position[appState.position.length - 1];
 
   const handleKeyDown = (e) => {
-
-    
-    
-    if(turn === 'b'){
+    if (turn === 'b') {
       return;
     }
+
     if (e.keyCode === 37 && column > 0) {
       setColumn(column - 1);
     } else if (e.keyCode === 39) {
       if (column === undefined) setColumn(0);
-      else if (column < 8) setColumn(column + 1);
-    } else if (e.keyCode === 32 ) {
-      
-      if (dropped.find((drop) => drop.x === 0 && drop.y === (column || 0))){
-        return;
+      else if (column < 8) {
+        setColumn(column + 1);
       }
-      else {
-        if (prevColumn !== null && column === prevColumn) {
-          return;
+    } else if (e.keyCode === 32) {
+      if (dropped.find((drop) => drop.x === 0 && drop.y === (column || 0))) {
+        return;
+      } else {
+        if (prevColumns !== null && column === prevColumns) {
+          return; // Can't drop in the same column as previous move
         }
+
         var len = 0;
-        while(len < 8 && position[7-len][(column || 0)] === ''){
-          len = len+1
+        while (len < 8 && position[7 - len][(column || 0)] === '') {
+          len = len + 1;
         }
-        if(len == 8)return;
-        len = len-1;
-        if(len == -1)return;
-        
+        if (len === 8) return;
+        len = len - 1;
+        if (len === -1) return;
+
         setTurn('b');
-        setPrevColumn(column);
+        setPrevColumns(column); // Update previous columns
         setRow(len);
         setTimeout(() => {
-          position[7-len][(column || 0)] = 'c'
+          position[7 - len][(column || 0)] = 'c';
           let newPosition = [
             ...dropped,
-            { x: len || 0, y: column || 0, player: turn }
-
+            { x: len || 0, y: column || 0, player: turn },
           ];
-  
+
           setDropped(newPosition);
-        },100);
-        
-        // console.log(col);
-        
+        }, 100);
       }
-      
     }
   };
 
   useEffect(() => {
     setColumn();
     setRow();
-   
   }, [turn]);
 
   useEffect(() => {
-    document.addEventListener("keyup", handleKeyDown, false);
-    return () => document.removeEventListener("keyup", handleKeyDown);
+    document.addEventListener('keyup', handleKeyDown, false);
+    return () => document.removeEventListener('keyup', handleKeyDown);
   });
 
   return (
     <div
-      className={`active p${turn} column-${column || "-"} row-${
-        row === undefined ? "-" : row
+      className={`active p${turn} column-${column || '-'} row-${
+        row === undefined ? '-' : row
       }`}
     />
   );
 };
 
-export default ActiveMarble;
+
+export default ActiveMarble
